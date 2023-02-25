dce27fdf39b264381f4bb297eec8727f2361fc2a
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-02-26 07:13:02 +0900

    add stock amount and validation creating product

--- product/src/main/java/com/sk/product/domain/entity/Product.java
+++ product/src/main/java/com/sk/product/domain/entity/Product.java
@@ -1,13 +1,15 @@
 package com.sk.product.domain.entity;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.springframework.util.Assert;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.Setter;
 
 @Data
@@ -17,12 +19,23 @@ public class Product {
 	private UUID id;
 	private String name;
 	private BigDecimal price;
+	private Long stockAmount;
 
-	public Product(UUID id, String name, BigDecimal price) {
+	@Builder
+	public Product(String name, BigDecimal price, Long stockAmount) {
+		this(UUID.randomUUID(), name, price, stockAmount);
+	}
+	
+	@Builder
+	public Product(UUID id, String name, BigDecimal price, Long stockAmount) {
 		Assert.hasText(name, "상품명은 필수 입니다.");
-		Assert.isTrue(price.compareTo(BigDecimal.ZERO) > 0, "가격은 0보다 커야 합니다.");
+		Assert.isTrue(Objects.requireNonNull(price)
+				.compareTo(BigDecimal.ZERO) > 0, "가격은 0보다 커야 합니다.");
+		Assert.isTrue(stockAmount > 0, "재고량은 0 이상이어야 합니다.");
 		this.id = id;
 		this.name = name;
 		this.price = price;
+		this.stockAmount = stockAmount;
 	}
+
 }
--- product/src/test/java/com/sk/product/domain/ProductServiceTest.java
+++ product/src/test/java/com/sk/product/domain/ProductServiceTest.java
@@ -1,6 +1,10 @@
 package com.sk.product.domain;
 
-import lombok.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,9 +15,6 @@ import com.sk.product.domain.entity.Product;
 import com.sk.product.domain.usecase.ProductPersistencePort;
 import com.sk.product.domain.usecase.ProductService;
 import com.sk.product.domain.usecase.ProductUsecase;
-
-import java.math.BigDecimal;
-import java.util.UUID;
 
 public class ProductServiceTest {
 
@@ -26,6 +27,20 @@ public class ProductServiceTest {
 		productUsecase = new ProductService(port);
     }
 	
+	@Test
+	@DisplayName("상품ID자동생성")
+	void makeProduct() throws Exception {
+		Product build = Product.builder().name("test").price(BigDecimal.ONE).stockAmount(10L).build();
+		Assertions.assertThat(build.getId()).isNotNull();
+	}
+	
+	@Test
+	@DisplayName("상품 생성시 가격 오류")
+	void makeProductErrorTest() throws Exception {
+		assertThrows(IllegalArgumentException.class, ()
+				-> new Product(UUID.randomUUID(), "상품", new BigDecimal(-10), 10L));
+	}
+	
     @Test
     @DisplayName("상품 등록하기")
     public void addProductTest(){
@@ -34,7 +49,7 @@ public class ProductServiceTest {
         String name = "실손보험";
 		BigDecimal price = BigDecimal.TEN;
 		
-		Product product = new Product(id, name, price);
+		Product product = new Product(id, name, price, 10L);
         //when
         productUsecase.register(product);
         Product find = productUsecase.findBy(id);
